cfac57a8e2aacbeb7a39696b81d508a53e92e6a6
hahalam204 <hahalam204@gmail.com>
2025-03-12 15:07:46 +0800

    Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
+import ReactDOM from 'react-dom/client';
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+
 
 export default function BasicButtons() {
     return (
@@ -10,3 +12,13 @@ export default function BasicButtons() {
         </Stack>
     );
 }
+class App extends React.Component {
+    render() {
+        return (
+            <BasicButtons></BasicButtons>
+        )
+    }
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#app'));
+root.render(<App />);
