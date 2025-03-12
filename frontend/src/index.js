3454070dda433910718a8dc05c9db0b3b25f3c28
hahalam204 <hahalam204@gmail.com>
2025-03-12 16:06:10 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import * as React from 'react';
+
+//import for dashboard
+import { extendTheme, styled } from '@mui/material/styles';
+import ChatIcon from '@mui/icons-material/Chat';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import HistoryIcon from '@mui/icons-material/History';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { PageContainer } from '@toolpad/core/PageContainer';
+import Grid from '@mui/material/Grid2';
+
+//import for the button
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
@@ -12,6 +24,121 @@ export default function BasicButtons() {
         </Stack>
     );
 }
+
+
+const NAVIGATION = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    segment: 'currentchatboard',
+    title: 'Current chat board',
+    icon: <ChatIcon />,
+  },
+  {
+    segment: 'newChat',
+    title: 'New chat',
+    icon: <OpenInNewIcon />,
+  },
+  {
+    kind: 'divider',
+  },
+  {
+    segment: 'history',
+    title: 'History',
+    icon: <HistoryIcon/>,
+  },
+];
+
+const demoTheme = extendTheme({
+  colorSchemes: { light: true, dark: true },
+  colorSchemeSelector: 'class',
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function useDemoRouter(initialPath) {
+  const [pathname, setPathname] = React.useState(initialPath);
+
+  const router = React.useMemo(() => {
+    return {
+      pathname,
+      searchParams: new URLSearchParams(),
+      navigate: (path) => setPathname(String(path)),
+    };
+  }, [pathname]);
+
+  return router;
+}
+
+const Skeleton = styled('div')(({ theme, height }) => ({
+  backgroundColor: theme.palette.action.hover,
+  borderRadius: theme.shape.borderRadius,
+  height,
+  content: '" "',
+}));
+
+export default function DashboardLayoutBasic(props) {
+  const router = useDemoRouter('/currentchatboard');
+
+  return (
+    <AppProvider
+      navigation={NAVIGATION}
+      router={router}
+      theme={demoTheme}
+    >
+      <DashboardLayout>
+        <PageContainer>
+          <Grid container spacing={1}>
+            <Grid size={5} />
+            <Grid size={12}>
+              <Skeleton height={14} />
+            </Grid>
+            <Grid size={12}>
+              <Skeleton height={14} />
+            </Grid>
+            <Grid size={4}>
+              <Skeleton height={100} />
+            </Grid>
+            <Grid size={8}>
+              <Skeleton height={100} />
+            </Grid>
+
+            <Grid size={12}>
+              <Skeleton height={150} />
+            </Grid>
+            <Grid size={12}>
+              <Skeleton height={14} />
+            </Grid>
+
+            <Grid size={3}>
+              <Skeleton height={100} />
+            </Grid>
+            <Grid size={3}>
+              <Skeleton height={100} />
+            </Grid>
+            <Grid size={3}>
+              <Skeleton height={100} />
+            </Grid>
+            <Grid size={3}>
+              <Skeleton height={100} />
+            </Grid>
+          </Grid>
+        </PageContainer>
+      </DashboardLayout>
+    </AppProvider>
+  );
+}
+
+
 class App extends React.Component {
     render() {
         return (
